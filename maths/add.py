be0f6f8ca882b8f392aaaaf361c1465a8ac9576c
SantiagoJTB <santitorresbravo@gmail.com>
2025-10-09 02:14:06 +0100

    Probando Actions

--- maths/add.py
+++ maths/add.py
@@ -1,26 +1,7 @@
 def add(*args):
     """
-        Suma un número indefinido de argumentos.
-
-        Parameters
-        ----------
-        *args : float
-            Números que se desean sumar. Se puede pasar 0 o más argumentos.
-
-        Returns
-        -------
-        float
-            La suma de todos los números. Si no se pasan argumentos, devuelve 0.
-
-        Examples
-        --------
-        >>> add(2, 3, 5)
-        10
-        >>> add(1)
-        1
-        >>> add()
-        0
-        FUNCIONAAAAAAAAAAAAAAAAAAAAAAAAAA
+        DANA TE QUIERO MUCHO
+        
         """
 
     return sum(args) if args else 0
