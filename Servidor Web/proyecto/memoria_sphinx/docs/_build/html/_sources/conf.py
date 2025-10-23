0ddde51a64616465ca4c047f38864242b49aa70f
SantiagoJTB <santitorresbravo@gmail.com>
2025-10-24 00:33:13 +0100

    prueba 3 actions

---
+++ Servidor Web/proyecto/memoria_sphinx/docs/_build/html/_sources/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))  # Apunta a la carpeta raíz que contiene 'math'
+
+project = 'sphinx_basics'
+copyright = '2025, santiago torres'
+author = 'santiago torres'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'sphinx_rtd_theme'  # Tema distinto al predeterminado
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
